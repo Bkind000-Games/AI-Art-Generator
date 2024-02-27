4d2104fe5162381bf68eaa7953a6ec1766a25c64
Bkind <bkind06@icloud.com>
2024-02-27 08:16:10 -0600

    Initial Commit

---
+++ AI Art Generator/AI_Art_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Art_GeneratorApp.swift
+//  AI Art Generator
+//
+//  Created by Kind, Braeden on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AI_Art_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Art Generator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AI Art Generator
+//
+//  Created by Kind, Braeden on 2/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
